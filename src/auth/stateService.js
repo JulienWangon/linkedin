c59eb6970798f14961edd28c5acab3bf1334b526
Julien Wangon <julien.wangon@gmail.com>
2024-10-10 20:25:49 +0200

    State storage service creation

---
+++ src/auth/stateService.js
@@ -0,0 +1,32 @@
+import * as SecureStore from 'expo-secure-store';
+import log from '../utils/logger';
+
+
+// Key to store the authentication state in SecureStore.
+const STATE_KEY = 'authState';
+
+
+/**
+ * Stores the authentication state securely.
+ * 
+ * @async
+ * @function storeState
+ * @param {string} state - The state string to store.
+ * @description This function saves the authentication state to Expo SecureStore and logs the result. 
+ * If an error occurs during storage, it logs the error message.
+ */
+
+export async function storeState(state) {
+  try {
+    // Store the provided state securely in SecureStore.
+    await SecureStore.setItemAsync(STATE_KEY, state);
+
+    // Log success message
+    log.info('State stored successfully:', state);
+
+  } catch (error) {
+
+    // Log any errors that occur during storage
+    log.debug('Erreur lors du stockage de l\'état:', error.message, error.stack);
+  }
+}

a6c7066bb1257c78872ddbd546e4bc773ae394cd
Julien Wangon <julien.wangon@gmail.com>
2024-10-11 09:29:59 +0200

    Creating screen navigation on app.js

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@reduxjs/toolkit";
+import LinkedinLoginScreen from "./src/screens/LinkedinLoginScreen";
+import { Home } from "./src/screens/Home";
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // The NavigationContainer manages the navigation structure of the app.
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LinkedinLoginScreen" component={LinkedinLoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
